1afa2c27aca9343b21ddb51acfa6bd6b357e6ec9
jabulane <jabumokoena85@gmail.com>
2023-06-07 15:00:38 -0700

    solved characters lengh  with a if statement if num ==99 dont add ,

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for num in range(00, 100):
-    print("{:02d}, ".format(num), end="")
-print("")
+    if num != 99:
+        print("{:02d}, ".format(num), end="")
+    else:
+        print("{:02d}".format(num))
