488692ee7e568a0593b784ef9e1c317bf47c67e2
jabulane <jabumokoena85@gmail.com>
2023-06-07 15:42:41 -0700

    adding a function that converts anf prints string to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def uppercase(string):
+    uppercase_string = ""
+
+    for char in string:
+        ascii_value = ord(char)
+
+        if 97 <= ascii_value <= 122:
+            uppercase_char = chr(ascii_value - 32)
+        else:
+            uppercase_char = char
+        uppercase_string += uppercase_char
+    print(uppercase_string)
+
