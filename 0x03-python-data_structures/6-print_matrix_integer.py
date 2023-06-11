8bf9a84ac5b6aefa53cf68fb706a5415c47a952f
jabulane <jabumokoena85@gmail.com>
2023-06-11 06:51:27 -0700

    changed code,so to not have spaces between last digits and $

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-
-    for outer_loop in matrix:
-        for in_loop in outer_loop:
-            print("{:d}".format(in_loop), end=' ')
-        print()
+    for row in matrix:
+        print(" ".join('{:d}'.format(col) for col in row))
