ddc7cb219661728aec0a831f848a0251ae13a704
jabulane <jabumokoena85@gmail.com>
2023-06-12 14:36:28 -0700

    fixed base case if the list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,6 +4,9 @@ def print_reversed_list_integer(my_list=[]):
 
     index = len(my_list) - 1
 
-    while index >= 0:
-        print("{:d}".format(my_list[index]))
-        index -= 1
+    if my_list is None or len(my_list) == 0:
+        return None
+    else:
+        while index >= 0:
+            print("{:d}".format(my_list[index]))
+            index -= 1
