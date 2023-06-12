ba648565098ada8d83ca174b015b99dac8478442
jabulane <jabumokoena85@gmail.com>
2023-06-12 12:42:35 -0700

    fixing the return value if sentence is empty, return tuple containing None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,4 +2,4 @@
 
 def multiple_returns(sentence):
 
-    return 0,"None" if len(sentence) == 0 else (len(sentence), sentence[0])
+    return (None) if len(sentence) == 0 else (len(sentence), sentence[0])
