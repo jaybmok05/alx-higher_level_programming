721ad3bfe9ab56af087fdccdc4b8d3d5750aa9f6
jabulane <jabumokoena85@gmail.com>
2023-06-14 11:25:29 -0700

    creating copy of the list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,12 +3,13 @@
 def uniq_add(my_list=[]):
     index = 0
     sum_nums = 0
+    new_list = my_list.copy()
 
-    while index < len(my_list):
-        if my_list.count(index) > 1:
-            my_list.remove(index)
+    while index < len(new_list):
+        if new_list.count(index) > 1:
+            new_list.remove(index)
 
-        sum_nums += my_list[index]
+        sum_nums += new_list[index]
         index += 1
 
     return sum_nums
