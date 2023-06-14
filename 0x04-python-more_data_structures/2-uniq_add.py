6bbc800f738834f52411074e8897883d99c72329
jabulane <jabumokoena85@gmail.com>
2023-06-14 11:31:59 -0700

    refactoring function to return sum of set

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    index = 0
-    sum_nums = 0
-    new_list = my_list.copy()
 
-    while index < len(new_list):
-        if new_list.count(index) > 1:
-            new_list.remove(index)
-
-        sum_nums += new_list[index]
-        index += 1
-
-    return sum_nums
+    return sum(set(num))
