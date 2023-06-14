eceb7160f1c90b21a8e1632dbe17c66cbcbc13b6
jabulane <jabumokoena85@gmail.com>
2023-06-14 10:52:42 -0700

    adding a function hat computes the square value of all integers of a matrix.

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+
+    new_list = []
+
+    for row in matrix:
+        new_list.append(list(map(lambda row: row**2, row)))
+
+    return new_list
