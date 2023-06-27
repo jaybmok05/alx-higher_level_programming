35a63f679ba7acc90cf76714a81389059a4de783
jabulane <jabumokoena85@gmail.com>
2023-06-27 14:27:08 -0700

    fixing pycode style

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,7 +29,7 @@ class Square:
             raise TypeError('size must be an integer')
         elif value < 0:
             raise ValueError('size must be >= 0')
-    
+
     @property
     def position(self):
         """Get/set the current position of the square."""
