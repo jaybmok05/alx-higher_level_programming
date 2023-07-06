052378ffc70141fa7b462ef908b1461bbcb6a490
jabulane <jabumokoena85@gmail.com>
2023-07-06 10:28:24 -0700

    a class that instantializes attributes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle"""
+
+
+class Rectangle():
+    """A class that defines private attributes"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >=0")
+        else:
+            self.__height = value
+            return self.__height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >=0")
+        else:
+            self.__width = value
+            return self.__width
