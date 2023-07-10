11ba909090bbb7f067acc0e2e60f4dcb17545ae6
jabulane <jabumokoena85@gmail.com>
2023-07-10 12:43:24 -0700

    adding 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,13 @@
 
 class Rectangle:
     """Represent a rectangle.
+
+    
+    Attributes:
+        number_of_instances
     """
+
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -12,6 +18,7 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
@@ -78,4 +85,5 @@ class Rectangle:
 
     def __del__(self):
         """a method that deletes an instance of rectangle"""
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
