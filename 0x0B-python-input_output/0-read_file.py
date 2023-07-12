69c3be18fd30aa457ce0b70ff09488a1b86d0ca8
jabulane <jabumokoena85@gmail.com>
2023-07-12 06:22:58 -0700

    adding 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Defining a function that reads a file'''
+
+
+def read_file(filename=""):
+    '''
+    A function that reads a text file and 
+    prints to stdout
+
+    Args:
+        filename(txt
+    '''
+    with open(filename, "r", encoding="utf-8") as fileobj:
+        fileobj.seek(0)
+        print(fileobj.read(), end='')
