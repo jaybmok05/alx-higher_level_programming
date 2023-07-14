6ee669e54df1979d601897f078f8f998645802e7
jabulane <jabumokoena85@gmail.com>
2023-07-14 10:48:37 -0700

    adding  4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Defining a function that returns an object'''
+import json
+
+
+def to_json_string(my_obj):
+    '''
+    a function that returns the JSON representation
+    of an object (string).
+
+    Args:
+        my_obj(dict): the obect
+
+    Return:
+         returns an object (Python data structure)
+         represented by a JSON string
+    '''
+    return json.loads(my_obj)
