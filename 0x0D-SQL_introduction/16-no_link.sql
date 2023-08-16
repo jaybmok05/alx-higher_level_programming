b40c1ca4e43f114057cc79d3d4428c44b74e62a0
jabulane <jabumokoena85@gmail.com>
2023-08-16 12:17:19 -0700

    adding 16-no_link.sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- lists all records of the table second_table of the database
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+SELECT score, name
+FROM second_table
+WHERE name != ""
+ORDER BY score DESC;
