e724a4aa83eb96d101ac87e41d48a3201f932511
jabulane <jabumokoena85@gmail.com>
2023-09-28 10:42:35 -0700

    adding task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
+def find_peak(list_of_integers):
+    """Finds the peak in a list of integers."""
+    if not list_of_integers:
+        return None
+
+    length = len(list_of_integers)
+    mid = length // 2
+    li = list_of_integers
+
+    if mid - 1 < 0 and mid + 1 >= length:
+        return li[mid]
+    elif mid - 1 < 0:
+        return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
+    elif mid + 1 >= length:
+        return li[mid] if li[mid] > li[mid - 1] else li[mid - 1]
+
+    if li[mid - 1] < li[mid] > li[mid + 1]:
+        return li[mid]
+
+    if li[mid + 1] > li[mid - 1]:
+        return find_peak(li[mid:])
+    else:
+        return find_peak(li[:mid])
